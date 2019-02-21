f3ddc273edaac63a0b2cb98372e16cb76599e466
Kat Maldon <katharinamaldon@gmail.com>
2019-02-21 19:41:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,13 @@
 class Student < User
   
+  attr_accessor :knowledge
+  
   def initialize 
     @knowledge = []
   end 
+ 
+   def learn(new_knowledge)
+    @knowledge << new_knowledge
+  end
 
 end
